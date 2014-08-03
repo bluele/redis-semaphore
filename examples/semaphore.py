a72c2012f1ca6af492e01e41410c7cc23c20142b
Jun Kimura <jksmphone@gmail.com>
2014-08-03 23:49:20 +0900

    Fixed example code.

--- examples/semaphore.py
+++ examples/semaphore.py
@@ -8,17 +8,18 @@ import time
 semaphore = Semaphore(Redis(), count=2, namespace='example')
 
 
-def task():
+def task(i):
     url = 'https://www.google.co.jp/'
     with semaphore:
-        print(urllib2.urlopen(url).code)
+        print('id: {} => {}'.format(i, urllib2.urlopen(url).code))
+        print('sleep...')
         time.sleep(2)
 
 
 def main():
     threads = list()
-    for _ in range(5):
-        threads.append(Thread(target=task))
+    for i in range(5):
+        threads.append(Thread(target=task, args=(i,)))
     for th in threads:
         th.start()
     for th in threads:
